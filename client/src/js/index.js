add2fbaf7813397ce0961b749961c36f5f4d7462
jiabinli <jiabinli@tencent.com>
2018-05-16 11:24:44 +0800

    build

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -44,42 +44,42 @@ function postfile(file) {
     var ot = new Date().getTime();
     var oloaded = 0
     xhr.upload.onprogress = function(evt) {
-        var progressBar = document.getElementById("progressBar");
+        // var progressBar = document.getElementById("progressBar");
         // var percentageDiv = document.getElementById("percentage");
         // event.total是需要传输的总字节，event.loaded是已经传输的字节。如果event.lengthComputable不为真，则event.total等于0
-        var html = ''
-        if (evt.lengthComputable) { //
-            progressBar.max = evt.total;
-            progressBar.value = evt.loaded;
-            html += ( Math.round(evt.loaded / evt.total * 100) + "%");
-        }
+        // var html = ''
+        // if (evt.lengthComputable) { //
+        //     // progressBar.max = evt.total;
+        //     // progressBar.value = evt.loaded;
+        //     html += ( Math.round(evt.loaded / evt.total * 100) + "%");
+        // }
 
-        var time = document.getElementById("time");
-        var nt = new Date().getTime(); //获取当前时间
-        var pertime = (nt - ot) / 1000; //计算出上次调用该方法时到现在的时间差，单位为s
-        ot = new Date().getTime(); //重新赋值时间，用于下次计算
+        // var time = document.getElementById("time");
+        // var nt = new Date().getTime(); //获取当前时间
+        // var pertime = (nt - ot) / 1000; //计算出上次调用该方法时到现在的时间差，单位为s
+        // ot = new Date().getTime(); //重新赋值时间，用于下次计算
 
-        var perload = evt.loaded - oloaded; //计算该分段上传的文件大小，单位b       
-        oloaded = evt.loaded; //重新赋值已上传文件大小，用以下次计算
+        // var perload = evt.loaded - oloaded; //计算该分段上传的文件大小，单位b       
+        // oloaded = evt.loaded; //重新赋值已上传文件大小，用以下次计算
 
-        //上传速度计算
-        var speed = perload / pertime; //单位b/s
-        var bspeed = speed;
-        var units = 'b/s'; //单位名称
-        if (speed / 1024 > 1) {
-            speed = speed / 1024;
-            units = 'k/s';
-        }
-        if (speed / 1024 > 1) {
-            speed = speed / 1024;
-            units = 'M/s';
-        }
-        speed = speed.toFixed(1);
-        //剩余时间
-        var resttime = ((evt.total - evt.loaded) / bspeed).toFixed(1);
-        time.innerHTML = html + '，速度：' + speed + units + '，剩余时间：' + resttime + 's';
-        if (bspeed == 0)
-            time.innerHTML = '上传已取消';
+        // //上传速度计算
+        // var speed = perload / pertime; //单位b/s
+        // var bspeed = speed;
+        // var units = 'b/s'; //单位名称
+        // if (speed / 1024 > 1) {
+        //     speed = speed / 1024;
+        //     units = 'k/s';
+        // }
+        // if (speed / 1024 > 1) {
+        //     speed = speed / 1024;
+        //     units = 'M/s';
+        // }
+        // speed = speed.toFixed(1);
+        // //剩余时间
+        // var resttime = ((evt.total - evt.loaded) / bspeed).toFixed(1);
+        // time.innerHTML = html + '，速度：' + speed + units + '，剩余时间：' + resttime + 's';
+        // if (bspeed == 0)
+        //     time.innerHTML = '上传已取消';
     }
     xhr.onload = function(e) { document.getElementById("time").innerHTML = (e.responseText) };
     xhr.send(formData);
